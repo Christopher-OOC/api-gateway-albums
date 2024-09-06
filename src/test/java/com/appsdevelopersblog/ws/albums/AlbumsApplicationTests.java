941dcd3e3f1949862d3b55fb3bae5b866d5e8cc3
Christopher-OOC <olojedechristopher24@gmail.com>
2024-09-06 10:16:03 +0100

    commit

---
+++ src/test/java/com/appsdevelopersblog/ws/albums/AlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdevelopersblog.ws.albums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
